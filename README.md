a878809c2d0bfd43c99b06aeb9d11f922806f653
Bellroute <kjkun7631@gmail.com>
2023-03-01 17:05:35 +0900

    init: 프로젝트 생성

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Numble_TimeDealServer
+
 [Numble 챌린지] Spring으로 타임 딜 서버 구축하기 
---
+++ src/main/java/com/numble/numble_timedealserver/NumbleTimeDealServerApplication.java
@@ -0,0 +1,13 @@
+package com.numble.numble_timedealserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleTimeDealServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbleTimeDealServerApplication.class, args);
+    }
+
+}
